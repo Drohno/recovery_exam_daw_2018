380c561947eec8ec3692da2fac3f10d6888ff827
iDev <david.roldan.nocelo@gmail.com>
2018-06-19 16:46:35 +0200

    uf1 ej1 hecho aunque falla el crear la tabla

--- UF1/exercici1/codi.js
+++ UF1/exercici1/codi.js
@@ -1,19 +1,23 @@
-function inici(){
+function inici() {
 
     var numero = document.getElementById('numero').value;
-    if(isNaN(numero)){
-        return window.alert('No has introduït un número');
+    try{
+        numero = parseInt(numero);
+    }catch(err){
+        window.alert('No has introduït un número');
     }
+    if (isNaN(numero)) {
+        window.alert('No has introduït un número');
+    } else {
 
-    
+        var resultado = '<table><tr><th>' + numero + '</th></tr>';
 
+        for (var i = 0; i < 10; i++) {
+            resultado += '<tr>'+ numero + 'x' + i + '=' + numero * i + '</tr>';
+        }
 
+        resultado += '</table>';
 
-
-
-
-
+        document.getElementById('resultat').innerHTML = resultado;
+    }
 }
-
-window.onload=inici;
-
