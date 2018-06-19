be8704650ca43d3130d919e72b0f82725c042bd4
iDev <david.roldan.nocelo@gmail.com>
2018-06-19 18:07:00 +0200

    UF2 ex3 done

--- UF2/exercici3/codi.js
+++ UF2/exercici3/codi.js
@@ -1,5 +1,34 @@
-function inici() {
+var llibre = {
+    id_llibre: null,
+    titol: null,
+    any: null,
+    infoAny: function () {
+        window.alert(this.any);
+    }
+};
 
+var llibres = [];
+
+function nou() {
+    var titol = prompt('¿Títol del llibre?', 'Sense nombre');
+    var any = prompt('Any del llibre?', 'dd/MM/yyyy');
+    llibre.id_llibre = llibres.length;
+    llibre.titol = titol;
+    llibre.any = any;
+    llibres.push(llibre);
+    document.getElementById('llistat').innerHTML += '<br>' + llibre.id_llibre + ' ' + llibre.titol + ' ' + llibre.any + '<br>';
 }
 
-window.onload = inici();
+function mostra_any() {
+    var id = prompt('ID llibre que vols saber l\'any', 0);
+    id = parseInt(id);
+    if(isNaN(id)){
+        window.alert('Input no vàlid');
+    }else{
+        try{
+            llibres[id].infoAny();
+        }catch(err){
+            window.alert('Llibre no disponible');
+        }
+    }
+}
