9836c66466607e4c3b4374bd63364cced16609ad
iDev <david.roldan.nocelo@gmail.com>
2018-06-19 17:48:54 +0200

    UF2 ex2 hecho

--- UF2/exercici2/codi.js
+++ UF2/exercici2/codi.js
@@ -1,5 +1,23 @@
 function inici() {
 
+    document.getElementById('link1').addEventListener('click', () => {
+        var respuesta = confirm('¿Ets major de 12 anys?');
+        if (respuesta) {
+            window.location.href = "https://www.starwars.com/films/solo";
+        } else {
+            window.location.href = "https://www.chiquipedia.com/peliculas-infantiles";
+        }
+    });
+
+    document.getElementById('link2').addEventListener('click', () => {
+        var respuesta = confirm('¿Ets major de 12 anys?');
+        if (respuesta) {
+            window.open('https://www.youtube.com/watch?v=PnJRSXhcZDw', 'targetWindow', 'toolbar=no,location = no,status = no, menubar = no,scrollbars = yes,resizable = no,width = 500px,height = 500px ');
+        } else {
+            window.alert('No pots accedir al trailer per ser menor de 12 anys');
+        }
+    });
+
 }
 
 window.onload = inici();
